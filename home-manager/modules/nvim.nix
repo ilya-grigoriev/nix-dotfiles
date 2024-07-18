cf99fbc7e9bba26704a56348dd469a7fabc50954
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2024-07-18 07:56:32 +0300

    refactor(nvim)

--- home-manager/modules/nvim.nix
+++ home-manager/modules/nvim.nix
@@ -1,18 +1,11 @@
 { config, pkgs, ... }:
 
-let
-  config_nvim = builtins.fetchGit {
-    url = "https://github.com/ilya-grigoriev/nvim";
-  };
+{
   programs.neovim = {
     enable = true;
     plugins = [
       pkgs.vimPlugins.lazy-nvim
+      pkgs.vimPlugins.luasnip
     ];
   };
-in
-  {
-    home.file.".config/nvim" = {
-      source = config.lib.file.mkOutOfStoreSymlink "~/.config/home-manager/nvim_config";
-    };
 }

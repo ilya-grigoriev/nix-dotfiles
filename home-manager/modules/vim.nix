04be67887529bfbb5129e4abd41d4c68f142b702
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2024-07-23 19:46:31 +0300

    feat(vim): changed config path

--- home-manager/modules/vim.nix
+++ home-manager/modules/vim.nix
@@ -4,8 +4,8 @@
   programs.vim = {
     enable = true;
     defaultEditor = true;
-    plugins = with pkgs.vimPlugins; [ jellybeans-vim  vimtex vim-snippets fzfWrapper ultisnips nerdcommenter vim-clang-format ];
+    plugins = with pkgs.vimPlugins; [ jellybeans-vim vimtex vim-snippets fzf-vim ultisnips nerdcommenter ale ];
     settings = { ignorecase = true; };
-    extraConfig = builtins.readFile ~/.config/home-manager/modules/vim/vimrc;
+    extraConfig = builtins.readFile ~/.vim/vimrc;
   };
 }

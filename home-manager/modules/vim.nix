912e525b3eb9814282914b24dd7b87bd4ad0b82b
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2024-08-14 21:56:41 +0300

    feat(vim): removed plugin

--- home-manager/modules/vim.nix
+++ home-manager/modules/vim.nix
@@ -4,7 +4,7 @@
   programs.vim = {
     enable = true;
     defaultEditor = true;
-    plugins = with pkgs.vimPlugins; [ jellybeans-vim vimtex vim-snippets fzf-vim ultisnips nerdcommenter ale ];
+    plugins = with pkgs.vimPlugins; [ jellybeans-vim vimtex vim-snippets fzf-vim ultisnips ale vim-surround];
     settings = { ignorecase = true; };
     extraConfig = builtins.readFile ~/.vim/vimrc;
   };
